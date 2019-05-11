fb82595c63c02c80e7be25d98762adac08af2a1c
FlowOverstack <ajdika@live.com>
2019-05-11 17:28:44 +0200

    Pass some faux data right into the this.people array inside the ngOnInit

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -12,5 +12,36 @@ export class CustomersComponent implements OnInit {
 
     ngOnInit() {
         this.title = 'Customers';
+
+        this.people = [
+            {
+                id: 1, 
+                name: 'John Doe', 
+                city: 'Phoenix', 
+                orderTotal: 9.99, 
+                customerSince: new Date(2014, 7, 10) 
+            },
+            {
+                id: 2, 
+                name: 'Jane Doe', 
+                city: 'Chandler', 
+                orderTotal: 9.99, 
+                customerSince: new Date(2017, 2, 22) 
+            },
+            {
+                id: 3, 
+                name: 'Michelle Thomas', 
+                city: 'Seattle', 
+                orderTotal: 9.99, 
+                customerSince: new Date(2004, 10, 31) 
+            },
+            {
+                id: 4, 
+                name: 'Jim Thomas', 
+                city: 'New York', 
+                orderTotal: 9.99, 
+                customerSince: new Date(2014, 10, 10) 
+            },                        
+        ]
     }
 }
