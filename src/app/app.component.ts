e7215bd172276a39a34d39d26d765a991849de9e
FlowOverstack <ajdika@live.com>
2019-05-11 17:09:03 +0200

    Hello World using data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dwscrimba';
+export class AppComponent implements OnInit {
+  title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    // We call a service that gets us the data
+    this.title = 'Hello World using data binding';
+  }
 }
