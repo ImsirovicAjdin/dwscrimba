7d44f1a6e8a3c1c888cb9ed475a53acd6c9d1392
FlowOverstack <ajdika@live.com>
2019-05-11 17:37:45 +0200

    Update CustomersModule so its code is no longer just a copy of AppModule

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,18 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+// you never import BrowserModule more than once in your app!
+import { CustomersComponent } from './customers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ CustomersComponent ],
+  imports: [ CommonModule ]
 })
-export class AppModule { }
+export class CustomersModule { }
