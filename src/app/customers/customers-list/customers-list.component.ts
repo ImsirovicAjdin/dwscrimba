33b76b001ec3b04c0a08e6785c04388ba8315477
FlowOverstack <ajdika@live.com>
2019-05-12 08:02:04 +0200

    Add variables to use in customers-list

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersListComponent implements OnInit {
 
-    
+    filteredCustomers: any[] = [];
+    customersOrderTotal: number;
+    currencyCode: string = 'USD';
+
     constructor() {}
     
     ngOnInit() {
