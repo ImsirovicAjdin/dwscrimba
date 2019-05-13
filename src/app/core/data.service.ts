18f7f144d8a786abaea2521dc6a6e0c8b1dd6667
FlowOverstack <ajdika@live.com>
2019-05-13 11:33:01 +0200

    Import some interfaces

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { map, catchError } from 'rxjs/operators';
 
-
+import { ICustomer, IOrder } from '../../app/shared/interfaces';
 
 @Injectable()
 export class DataService {
