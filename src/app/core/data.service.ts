e3c2df69481ad332d888324682ff5fafbf4db0a3
FlowOverstack <ajdika@live.com>
2019-05-13 11:38:42 +0200

    Create private hnadleError function which will console out the errors, and ALSO allow them to be thrown back to the caller

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -8,9 +8,23 @@ import { ICustomer, IOrder } from '../../app/shared/interfaces';
 
 @Injectable()
 export class DataService {
-    
-    baseUrl: string = 'assets/';
 
-    constructor() {}
+    baseUrl: string = 'assets/';
+    
+    constructor() { }
+
+
+
+
+    private handleError(error: any) {
+      console.error('server error:', error);
+      if (error.error instanceof Error) {
+          const errMessage = error.error.message;
+          return Observable.throw(errMessage);
+          // Use the following instead if using lite-server
+          // return Observable.throw(err.text() || 'backend server error');
+      }
+      return Observable.throw(error || 'Node.js server error');
+    }
 
 }
