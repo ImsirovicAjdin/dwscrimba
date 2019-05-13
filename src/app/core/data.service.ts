5af76e1358f778f53e00c5c915b4c36f758c9242
FlowOverstack <ajdika@live.com>
2019-05-13 12:14:45 +0200

    Returning async data with observables, pt1

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -9,10 +9,22 @@ import { ICustomer, IOrder } from '../../app/shared/interfaces';
 @Injectable()
 export class DataService {
 
-    baseUrl: string = 'assets/';
+    baseUrl: string = 'assets/'; // we'll just call a json file, but we can just as easily call a url, just replace the string here
     
     constructor(private http: HttpClient) { }
 
+    getCustomers() : Observable<ICustomer[]> {
+        // a call to getCustomers() will return an ICustomer array (<ICustomer[]), but it's an async operation so we don't know how long it will take
+        // the Observable will allow us to return async data
+        // Essentially, what it does is, it starts an operation, and then someone could say, 'Hey, I wanna subscribe to getting customers'
+        // ... and when we return this observable they will subscribe to it, and once the data eventually comes back from the server, they'll then get it
+        // ... then that subscription can kind of end and you can unsubscribe
+
+        // we're gonna use this http client to actually call the server, get our customers json, and then return it to a component that's gonna subscribe by calling this customers function
+        // so we're gonna return an observable, and this is gonna be done by calling this.http.get
+        // get will allow us, optionally, to say 'Hey, what is the type of data you're expecting to get?'; Well, we want an ICustomer array:
+            return this.http.get<ICustomer[]>
+    }
 
 
 
