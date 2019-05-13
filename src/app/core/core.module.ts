d05d3ea49ff360701bbc7242418fbac795131ea7
FlowOverstack <ajdika@live.com>
2019-05-13 12:02:37 +0200

    Remove comments

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,10 +7,5 @@ import { SorterService } from './sorter.service';
 @NgModule({
     imports: [ ],
     providers: [ DataService, SorterService ]
-    // provider goes hand-in-hand with an injector container
-    // the injector container will contact the provider (the 'chef'), and say 'Hey, I need a data service instance'
-    // it will pass it to the injector container, which will then store it as a singleton, and then the injector container will pass that into whoever requested the object
-    // So I kind of think of the provider as the chef, the injector container as a waiter
-
 })
 export class CoreModule { }
