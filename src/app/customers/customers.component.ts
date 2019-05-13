6e29d8e9e4e742b5c3ca5f815677bca703b770fe
FlowOverstack <ajdika@live.com>
2019-05-13 13:42:15 +0200

    Import DataService into customers.component.ts

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ICustomer } from '../shared/interfaces';
+import { DataService } from '../core/data.service';
 
 @Component({
     selector: 'app-customers',
