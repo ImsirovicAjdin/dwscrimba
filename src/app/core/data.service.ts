7271d738206349b71eb5b3bcac56fcfef4511c80
FlowOverstack <ajdika@live.com>
2019-05-13 13:10:33 +0200

    Pipes are kind of like filters

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -17,7 +17,16 @@ export class DataService {
         // and now we'll just pass to .get the data source
         // now we could do that, but it's possible we might get an error. Maybe the server's down,
         // or the network's down, or something else could go haywire
-            return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json');
+        // and how operators work, we have a pipe, if you imagine data as a hose with water flowing through,
+        // a pipe is a filter we can plug into the pipe, and these filters in our case are called operators
+        // and what we can do is then, plug in an operator to handle errors
+        // and if an error flows through the hose, the pipe, then we can plug sth into the pipeline that will plop that err off,
+        // and then we can send it down to the handleError() that is under the getCustomers() 
+        // so we're gonna ad a catchError operator that we imported on line 5
+            return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json')
+            .pipe(
+                catchError(this.handleError)
+            );
     }
 
 
