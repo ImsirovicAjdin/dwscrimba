fcc22e0a74958be5c71734437c15529fe5ece77e
FlowOverstack <ajdika@live.com>
2019-05-13 11:32:17 +0200

    Import more symbols from rxjs

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
+import { map, catchError } from 'rxjs/operators';
+
+
 
 @Injectable()
 export class DataService {
