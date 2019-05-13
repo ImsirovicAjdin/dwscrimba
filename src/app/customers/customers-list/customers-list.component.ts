1966b4678d635fd1303ae7a3ed062e29156e2957
FlowOverstack <ajdika@live.com>
2019-05-13 15:18:49 +0200

    Remove comments

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -7,18 +7,17 @@ import { ICustomer } from '../../shared/interfaces';
     templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit {
-    // instead, we'll use the property getters and setters technique, 
-    // made available by ES2015
+
     private _customers: ICustomer[] = [];
-    // I'm gonna add a get block here
+
     @Input() get customers(): ICustomer[] {
         return this._customers;
     }
 
     set customers(value: ICustomer[]) {
         if (value) {
-            this.filteredCustomers = this._customers = value; // (!)
-            this.calculateOrders(); // this will iterate thru the customers and do sth with those
+            this.filteredCustomers = this._customers = value;
+            this.calculateOrders();
         }
     }
 
@@ -48,15 +47,11 @@ export class CustomersListComponent implements OnInit {
         } else {
             this.filteredCustomers = this.customers;
         }
-        this.calculateOrders(); // Make it always recalculate the orders to avoid the calculate orders bug        
+        this.calculateOrders();       
     }
 
 
     sort(prop: string) {
-        // A sorter service will handle the sorting
-        // ... so really, all we need to know in a component, such as our customers list component here, is that we need, in our sort() function, to build a call to that
-        // ... sorter service.sort, pass in our filtered customers that we got on line 20 of this file in (!) 
-        // ... and then get this sorted data back and display it
     }
 
 }
