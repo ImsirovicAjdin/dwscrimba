4ed06826b8d742d516992f108e18ded92be02215
FlowOverstack <ajdika@live.com>
2019-05-13 14:54:44 +0200

    Fully explain how to subscribe to an observable inside the customers component

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -17,20 +17,19 @@ export class CustomersComponent implements OnInit {
         this.isVisible = !this.isVisible;
     }
 
-    constructor(private dataService: DataService) {
-        // Now we want to use the injected DataService to actually call into the getCustomers function, and then subscribe to this Observable in the getCustomers function, to get back this customers data
-        // and we'll do that in (1) here:
-    }
+    constructor(private dataService: DataService) { }
 
     ngOnInit() {
         this.title = 'Customers Test';
-        // This: this.dataService.getCustomers();  ...returns an observable, so we can't do this:
-        // this.people = this.dataService.getCustomers();
-        // instead, what we have to do, is tell the getCustomers to go to the server and get the data:
-        // this.dataService.getCustomers(); // and that's because getCustomers will get what's called a "cold observable" - a cold observable is the one that's ready to go, but you have to turn it on, otherwise it just sits there dormant and doesn't do anything
 
         this.dataService.getCustomers()
-            .subscribe(); // of course, with the code like this, we would have no way to get the data back
+            .subscribe((customers: ICustomer[]) => this.people = customers); // we wanna get back data, and we wanna get back customers, which we're gonna assign to the people property
+                                //  so we're gonna pass those (customers: ICustomer[]) that this subscribe gives to us, and we assign it to the this.people property
+        // So as a quick review,  the dataService has a getCustomers(), and it returns an observable. An observable is basically an async operation, saying "I'm gonna get to the data, but it might be a couple of seconds, 
+        // it might be a second, we don't know - it depends on the network and a bunch of other factors"
+        // we're then gonna take that observable, and subscribe to it; that allows us to get the customers: ICustomer[] data, EVENTUALLY, from the async operation;
+        // and then we're gonna go ahead and assign this customers data to this.people: ... => this.people = customers
+        // Now if everything is working properly, and we have the providers for the service and our modules are setup right, and our data service works, then now when we refresh we should get some data here
         /*
         this.people = [
             {
